8a7131cf27bc0a9517cbb09ae05677646e7da877
LeonJose <joseluisleondamas@gmail.com>
2016-07-27 23:03:43 -0700

    Se creo controlador y dos vistas main y footer

---
+++ application/views/main.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+   <meta name="description" content="Bootstrap Admin App + jQuery">
+   <meta name="keywords" content="app, responsive, jquery, bootstrap, dashboard, admin">
+   <title>Angle - Bootstrap Admin Template</title>
+   <!-- =============== VENDOR STYLES ===============-->
+   <!-- FONT AWESOME-->
+   <link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
+   <!-- SIMPLE LINE ICONS-->
+   <link rel="stylesheet" href="vendor/simple-line-icons/css/simple-line-icons.css">
+   <!-- ANIMATE.CSS-->
+   <link rel="stylesheet" href="vendor/animate.css/animate.min.css">
+   <!-- WHIRL (spinners)-->
+   <link rel="stylesheet" href="vendor/whirl/dist/whirl.css">
+   <!-- =============== PAGE VENDOR STYLES ===============-->
+   <!-- WEATHER ICONS-->
+   <link rel="stylesheet" href="vendor/weather-icons/css/weather-icons.min.css">
+   <!-- =============== BOOTSTRAP STYLES ===============-->
+   <link rel="stylesheet" href="vendor/site/css/bootstrap.css" id="bscss">
+   <!-- =============== APP STYLES ===============-->
+   <link rel="stylesheet" href="vendor/site/css/site/app.css" id="maincss">
+
+    <link rel="stylesheet" href="vendor/site/css/app.css" id="maincss">
+    <base href="<?= base_url() ?>"></base>
+
+</head>
+
+<body>
+
+   <div class="wrapper">
+      <!-- top navbar-->
+      <header class="topnavbar-wrapper">
+         <!-- START Top Navbar-->
+         <nav role="navigation" class="navbar topnavbar">
+            <!-- START navbar header-->
+            <div class="navbar-header">
+               <a href="#/" class="navbar-brand">
+                  <div class="brand-logo">
+                     <img src="img/logo.png" alt="App Logo" class="img-responsive">
+                  </div>
+                  <div class="brand-logo-collapsed">
+                     <img src="img/logo-single.png" alt="App Logo" class="img-responsive">
+                  </div>
+               </a>
+            </div>
+            <!-- END navbar header-->
+            <!-- START Nav wrapper-->
+            <div class="nav-wrapper">
+               <!-- START Left navbar-->
+               <ul class="nav navbar-nav">
+                  <li>
+                     <!-- Button used to collapse the left sidebar. Only visible on tablet and desktops-->
+                     <a href="#" data-trigger-resize="" data-toggle-state="aside-collapsed" class="hidden-xs">
+                        <em class="fa fa-navicon"></em>
+                     </a>
+                     <!-- Button to show/hide the sidebar on mobile. Visible on mobile only.-->
+                     <a href="#" data-toggle-state="aside-toggled" data-no-persist="true" class="visible-xs sidebar-toggle">
+                        <em class="fa fa-navicon"></em>
+                     </a>
+                  </li>
+               </ul>
+               <!-- END Left navbar-->
+            </div>
+            <!-- END Nav wrapper-->
+            <!-- START Search form-->
+            <form role="search" action="search.html" class="navbar-form">
+               <div class="form-group has-feedback">
+                  <input type="text" placeholder="Type and hit enter ..." class="form-control">
+                  <div data-search-dismiss="" class="fa fa-times form-control-feedback"></div>
+               </div>
+               <button type="submit" class="hidden btn btn-default">Submit</button>
+            </form>
+            <!-- END Search form-->
+         </nav>
+         <!-- END Top Navbar-->
+      </header>
+      <!-- sidebar-->
+         <aside class="aside">
+            <!-- START Sidebar (left)-->
+            <div class="aside-inner">
+               <nav data-sidebar-anyclick-close="" class="sidebar">
+                  <!-- START sidebar nav-->
+                  <ul class="nav">
+                     <!-- Iterates over all sidebar items-->
+                     <li class="nav-heading ">
+                        <span data-localize="sidebar.heading.HEADER">Main Navigation</span>
+                     </li>
+
+                     <li class=" ">
+                        <a href="widgets.html" title="Widgets">
+                           <div class="pull-right label label-success">30</div>
+                           <em class="icon-grid"></em>
+                           <span data-localize="sidebar.nav.WIDGETS">Widgets</span>
+                        </a>
+                     </li>  
+                     </ul>
+                  </nav> 
+               </div>        
+         <!-- END Off Sidebar (right)-->
+         </aside>
+
+               <!-- Main section-->
+               <section>
+                  <!-- Page content-->
+                  <div class="content-wrapper">
+                     <div class="content-heading">
+                        Sistema 
+                        <small data-localize="dashboard.WELCOME"></small>
+                     </div>
+                  </div>
+               </section>
